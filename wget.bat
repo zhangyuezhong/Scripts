7a99ded39f638a4fe2f53dd473ed3d5c689e316e
Yuezhong Zhang <zhangyuezhong@users.noreply.github.com>
2017-07-01 22:04:17 +1000

    Add files via upload

---
+++ wget.bat
@@ -0,0 +1,40 @@
+@echo off
+
+rem Windows has no built-in wget or curl, so generate a VBS script to do it:
+rem -------------------------------------------------------------------------
+set DLOAD_SCRIPT=%TEMP%\download.vbs
+del %DLOAD_SCRIPT%
+echo Option Explicit                                                    >  %DLOAD_SCRIPT%
+echo Dim args, http, fileSystem, adoStream, url, target, status         >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo Set args = Wscript.Arguments                                       >> %DLOAD_SCRIPT%
+echo Set http = CreateObject("WinHttp.WinHttpRequest.5.1")              >> %DLOAD_SCRIPT%
+echo url = args(0)                                                      >> %DLOAD_SCRIPT%
+echo target = args(1)                                                   >> %DLOAD_SCRIPT%
+echo WScript.Echo "Downloading '" ^& url ^& "' to '" ^& target ^& "'..."  >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo http.Open "GET", url, False                                        >> %DLOAD_SCRIPT%
+echo http.Send                                                          >> %DLOAD_SCRIPT%
+echo status = http.Status                                               >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo If status ^<^> 200 Then                                            >> %DLOAD_SCRIPT%
+echo 	WScript.Echo "FAILED to download: HTTP Status " ^& status       >> %DLOAD_SCRIPT%
+echo 	WScript.Quit 1                                                  >> %DLOAD_SCRIPT%
+echo End If                                                             >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo Set adoStream = CreateObject("ADODB.Stream")                       >> %DLOAD_SCRIPT%
+echo adoStream.Open                                                     >> %DLOAD_SCRIPT%
+echo adoStream.Type = 1                                                 >> %DLOAD_SCRIPT%
+echo adoStream.Write http.ResponseBody                                  >> %DLOAD_SCRIPT%
+echo adoStream.Position = 0                                             >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+echo Set fileSystem = CreateObject("Scripting.FileSystemObject")        >> %DLOAD_SCRIPT%
+echo If fileSystem.FileExists(target) Then fileSystem.DeleteFile target >> %DLOAD_SCRIPT%
+echo adoStream.SaveToFile target                                        >> %DLOAD_SCRIPT%
+echo adoStream.Close                                                    >> %DLOAD_SCRIPT%
+echo.                                                                   >> %DLOAD_SCRIPT%
+rem -------------------------------------------------------------------------
+
+cscript //Nologo %DLOAD_SCRIPT% http://apache.melbourneitmirror.net//ant/binaries/apache-ant-1.10.1-bin.zip C:\sdk\dist\apache-ant-1.10.1-bin.zip
+echo File downloaded
+del %DLOAD_SCRIPT%
